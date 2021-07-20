0d17121d831215c690556c99283cf584376196ef
Muhammad Hassan Ahmed <33365802+hassan11196@users.noreply.github.com>
2021-07-20 10:47:43 +0000

    remove require js

--- jupyter_notebook_config.py
+++ jupyter_notebook_config.py
@@ -1,2 +1,2 @@
 import os
-c.NotebookApp.extra_static_paths = ['{}/js/'.format(os.environ['ROOTSYS']), '/usr/local/lib/python3.8/dist-packages/notebook/static/components/requirejs']
+c.NotebookApp.extra_static_paths = ['{}/js/'.format(os.environ['ROOTSYS']), ]
